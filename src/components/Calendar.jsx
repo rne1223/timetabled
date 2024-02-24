d7f4c1996e3903a7cf4b6d646edbcd7b8b007d04
rne1223 <rne1223@gmail.com>
2024-02-23 16:32:10 -0800

    Finished the Stretch Feature

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -18,12 +18,12 @@ const Calendar = () => {
       <tbody>
         <tr>
           <td className="time">8 am</td>
-          <Event event="Fancy Dinner 🎩" color="green" />
           <td></td>
           <td></td>
+          <Event event="Fancy 🎩" color="green"  location='Hat Store'/>
           <td></td>
           <td></td>
-          <Event event="Yolk 🍳" color="green" />
+          <Event event="Yolk 🍳" color="green" location='Church'/>
           <td></td>
         </tr>
 
@@ -32,16 +32,16 @@ const Calendar = () => {
           <td></td>
           <td></td>
           <td></td>
-          <Event event="Subway 🚊" color="pink" />
+          <Event event="Subway 🚊" color="pink" location='Eat Fresh'/>
           <td></td>
           <td></td>
-          <Event event="The Bean 🫘" color="blue" />
+          <Event event="The Bean 🫘" color="blue" location='Tia'/>
         </tr>
 
         <tr>
           <td className="time">10 am</td>
           <td></td>
-          <Event event="Subway 🚊" color="pink" />
+          <Event event="Subway 🚊" color="pink" location='Eat Fresh'/>
           <td></td>
           <td></td>
           <td></td>
@@ -52,7 +52,7 @@ const Calendar = () => {
         <tr>
           <td className="time">11 am</td>
           <td></td>
-          <Event event="Deep Dish 🍕" color="blue" />
+          <Event event="Deep Dish 🍕" color="blue" location='Dominos'/>
           <td></td>
           <td></td>
           <td></td>
@@ -64,11 +64,11 @@ const Calendar = () => {
           <td className="time">12 pm</td>
           <td></td>
           <td></td>
-          <Event event="Subway 🚊" color="pink" />
+          <Event event="Subway 🚊" color="pink" location='Eat Fresh'/>
           <td></td>
           <td></td>
           <td></td>
-          <Event event="Cubs Game ⚾" color="green" />
+          <Event event="Cubs Game ⚾" color="green" location='San Francisco'/>
         </tr>
 
         <tr>
@@ -76,7 +76,7 @@ const Calendar = () => {
           <td></td>
           <td></td>
           <td></td>
-          <Event event="Fancy Dinner 🍚" color="green" />
+          <Event event="Fancy Dinner 🍚" color="green" location='Chinatown'/>
           <td></td>
           <td></td>
           <td></td>
@@ -85,12 +85,12 @@ const Calendar = () => {
         <tr>
           <td className="time">2 pm</td>
           <td></td>
-          <Event event="Coffee 🍵" color="yellow" />
+          <Event event="Coffee 🍵" color="yellow" location='Home'/>
           <td></td>
           <td></td>
           <td></td>
           <td></td>
-          <Event event="Subway 🚊" color="pink" />
+          <Event event="Subway 🚊" color="pink" location='Eat Fresh'/>
         </tr>
 
         <tr>
@@ -100,13 +100,13 @@ const Calendar = () => {
           <td></td>
           <td></td>
           <td></td>
-          <Event event="Subway 🚊" color="pink" />
+          <Event event="Subway 🚊" color="pink" location='Eat Fresh'/>
           <td></td>
         </tr>
 
         <tr>
           <td className="time">4 pm</td>
-          <Event event="Subway 🚊" color="green" />
+          <Event event="Subway 🚊" color="green" location='Eat Fresh'/>
           <td></td>
           <td></td>
           <td></td>
@@ -119,9 +119,9 @@ const Calendar = () => {
           <td className="time">5 pm</td>
           <td></td>
           <td></td>
-          <Event event="Family Time 👪" color="yellow" />
+          <Event event="Family Time 👪" color="yellow" location='Home'/>
           <td></td>
-          <Event event="Subway 🚊" color="pink" />
+          <Event event="Subway 🚊" color="pink" location='Eat Fresh'/>
           <td></td>
           <td></td>
         </tr>
